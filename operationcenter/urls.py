b487f0a5b9107d9fbdda2fc499df0804e33052b0
stevescilar <stevemuambi@gmail.com>
2022-02-19 01:28:29 +0300

    off

--- operationcenter/urls.py
+++ operationcenter/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
     path('/about',views.about, name='about'),
-    path('/china',views.china, name='china')
+    # path('/china',views.china, name='china'),
 ]
